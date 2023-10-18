dd95f409176e8740eb6c9ed07cf56451ef9ff9aa
safdar211ali <safdar211@hotmail.com>
2023-10-18 17:22:20 +0500

    updated report again

--- tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.py
+++ tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.py
@@ -101,8 +101,10 @@ class AccountsReceivableSummary(ReceivablePayableReport):
 			# Add all amount columns
 			for k in list(self.party_total[d.party]):
 				if k not in ["currency", "sales_person"]:
-
-					self.party_total[d.party][k] += d.get(k, 0.0)
+					if self.party_total[d.party][k] is not None:
+						self.party_total[d.party][k] += d.get(k, 0.0)
+					else:
+						self.party_total[d.party][k] = d.get(k, 0.0)
 
 			# set territory, customer_group, sales person etc
 			self.set_party_details(d)
