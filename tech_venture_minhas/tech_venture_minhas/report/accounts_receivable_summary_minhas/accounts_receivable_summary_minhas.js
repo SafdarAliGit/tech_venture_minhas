7088c2843bd6f16f1249442ec6e78f07c3d64e80
safdar211ali <safdar211@hotmail.com>
2023-10-19 07:52:13 +0500

    query updet

--- tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.js
+++ tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.js
@@ -2,7 +2,12 @@
 
 frappe.query_reports["Accounts Receivable Summary Minhas"] = {
 	"filters": [
-
+{
+			"fieldname":"report_date",
+			"label": __("Posting Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
 	]
 
 };
--- tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.py
+++ tech_venture_minhas/tech_venture_minhas/report/accounts_receivable_summary_minhas/accounts_receivable_summary_minhas.py
@@ -11,30 +11,31 @@ from frappe.utils import nowdate, getdate, flt
 
 
 def execute(filters=None):
-    return get_columns(filters), get_data()
+    return get_columns(filters), get_data(filters)
 
 
-def get_data():
+def get_data(filters):
     data = frappe.db.sql(
         """
         SELECT 
             gle.party, 
-            cust.customer_group, 
-            gle.account_currency AS currency, 
-            SUM(gle.debit) - SUM(gle.credit) AS outstanding
+            SUM(gle.debit) - SUM(gle.credit) AS outstanding,
+             cust.customer_group, 
+            gle.account_currency AS currency
         FROM 
             `tabGL Entry` gle
         JOIN
-            `tabCustomer` cust ON cust.name = gle.party  -- Joining with the Customer table
+            `tabCustomer` cust ON cust.name = gle.party  
         WHERE 
             gle.posting_date <= %(nowdate)s 
             AND gle.docstatus = 1
         GROUP BY 
             gle.party
         """,
-        {"nowdate": nowdate()}
+        {"nowdate": filters.get("report_date")},as_dict=1,
     )
-    return data
+    filtered_array = [d for d in data if d.get('outstanding') != 0]
+    return filtered_array
 
 
 def get_columns(filters):
@@ -47,22 +48,22 @@ def get_columns(filters):
             "width": 200
         },
         {
+            "fieldname": "outstanding",
+            "fieldtype": "Currency",
+            "label": "Outstanding",
+            "width": 150
+        },
+        {
             "fieldname": "customer_group",
             "fieldtype": "Data",
             "label": "Customer Group",
             "width": 150
 
         },
-    {
-        "fieldname": "currency",
-        "fieldtype": "Data",
-        "label": "Currency",
-        "width": 150
-    },
         {
-            "fieldname": "outstanding",
-            "fieldtype": "Currency",
-            "label": "Outstanding",
+            "fieldname": "currency",
+            "fieldtype": "Data",
+            "label": "Currency",
             "width": 150
         }
     ]
