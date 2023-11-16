3894212114880fa81129aabb6a235c7c07cc6b62
safdar211ali <safdar211@hotmail.com>
2023-11-16 22:22:45 +0500

    Updated

--- tech_venture_minhas/tech_venture_minhas/report/variant_stock_summary/variant_stock_summary.py
+++ tech_venture_minhas/tech_venture_minhas/report/variant_stock_summary/variant_stock_summary.py
@@ -119,14 +119,18 @@ def get_data(filters):
                     `tabItem`.variant_of,
                     SUM(`tabStock Ledger Entry`.qty_after_transaction) AS qty_after_transaction
                 FROM 
-                    `tabItem`,`tabStock Ledger Entry`
-                WHERE `tabItem`.item_code = `tabStock Ledger Entry`.item_code 
+                    `tabItem`, `tabStock Ledger Entry`
+                WHERE 
+                    `tabItem`.item_code = `tabStock Ledger Entry`.item_code 
                     AND `tabStock Ledger Entry`.docstatus < 2 
                     AND `tabStock Ledger Entry`.is_cancelled = 0 
                     AND {conditions}
                 GROUP BY
                     `tabItem`.variant_of
-                """.format(conditions=get_other_conditions(filters, "Stock Ledger Entry"))
+                ORDER BY
+                    `tabStock Ledger Entry`.posting_date
+                LIMIT 1;    
+            """.format(conditions=get_other_conditions(filters, "Stock Ledger Entry"))
     other_stock_result = frappe.db.sql(other_stock_query, filters, as_dict=1)
 
     for item in stock_result:
